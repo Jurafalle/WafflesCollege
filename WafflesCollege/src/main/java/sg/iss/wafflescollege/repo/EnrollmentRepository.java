f87d5bd6e7fe25e5df009c44b9ce2ce57b8a0a78
yaoyao96 <jyyaoyaoyao@gmail.com>
2018-12-11 18:14:35 +0800

    22

--- WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/EnrollmentRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.iss.wafflescollege.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.iss.wafflescollege.model.Enrollment;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment,Integer> {
-
-}
