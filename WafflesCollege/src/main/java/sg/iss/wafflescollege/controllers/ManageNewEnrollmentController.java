2d0c66f6730d1a4a8243f518975a4340df4ed7ae
Ruth <e0336184@u.nus.edu>
2018-12-12 11:28:22 +0800

    ruth edits

--- WafflesCollege/src/main/java/sg/iss/wafflescollege/controllers/ManageNewEnrollmentController.java
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/controllers/ManageNewEnrollmentController.java
@@ -10,41 +10,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 import sg.iss.wafflescollege.model.Enrollment;
 import sg.iss.wafflescollege.model.Student;
 import sg.iss.wafflescollege.services.EnrollmentService;
-
+import sg.iss.wafflescollege.validator.StudentValidator;
 
 
 @RequestMapping(value = "/admin/managenewenrollment")
 @Controller
 public class ManageNewEnrollmentController {
-	@Autowired
-	private EnrollmentService cService;
-
 	
-	@InitBinder
-	private void initBinder(WebDataBinder binder) {
-		//binder.setValidator(studentValidator);
-	}
-	@RequestMapping(value = "/new")
-	public ModelAndView pendingApprovals(HttpSession session) {
-		HashMap<Student, ArrayList<Enrollment>> hm = new HashMap<Student, ArrayList<Enrollment>>();
-		UserSession us = (UserSession) session.getAttribute("USERSESSION");
-		System.out.println(us.toString());
-		ModelAndView mav = new ModelAndView("MainPage");
-		if (us.getSessionId() != null) {
-			for (Student student : us.getStudent()) {
-				ArrayList<Enrollment> clist = cService.findNewEnrollmentById(student.getStuId());
-				hm.put(student, clist);
-		}
-			mav = new ModelAndView("ManageNewEnrollment");
-			mav.addObject("pendinghistory", hm);
-			return mav;
-		}
+	@Autowired
+	private EnrollmentService eService;
+
+
+	@RequestMapping(value = "/new", method=RequestMethod.GET)
+	public ModelAndView listAll() {
+		ArrayList<Enrollment> enrollment = eService.findAllNewEnrollments();
+		ModelAndView mav = new ModelAndView("ManageNewEnrollment");
+		mav.addObject("enrollment", enrollment);
 		return mav;
-	}
+		}
 }
