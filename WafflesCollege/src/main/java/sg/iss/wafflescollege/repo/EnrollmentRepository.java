c51ca4dc2452b10866dc7ddcac86f9e697f14bd0
yaoyao96 <jyyaoyaoyao@gmail.com>
2018-12-12 10:57:32 +0800

    enrollment repo

--- WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/EnrollmentRepository.java
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/EnrollmentRepository.java
@@ -13,9 +13,15 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment,Integer> 
 	@Query("select e from Enrollment e where e.enrStatus ='Pending'")
 	ArrayList<Enrollment> findAllNewEnrollments();
 	
-	@Query("select e from Enrollment e where e.enrStatus ='Pending' and e.stuId=:sid")
+	@Query("select e from Enrollment e where e.enrStatus ='Pending' and e.student=:sid")
 	ArrayList<Enrollment> findNewEnrollmentBysID(@Param("sid") String sid);
 
-	@Query("select e from Enrollment e where e.enrStatus ='Pending'e.stuId=:sid")
+	@Query("select e from Enrollment e where e.enrStatus ='Pending' and e.course=:cid")
+	ArrayList<Enrollment> findNewEnrollmentBycID(@Param("cid") String cid);
+	
+	@Query("select e from Enrollment e where e.student=:sid")
 	ArrayList<Enrollment> findEnrollmentBysID(@Param("sid") String sid);
+	
+	@Query("select e from Enrollment e where e.course=:cid")
+	ArrayList<Enrollment> findEnrollmentBycID(@Param("cid") String cid);
 }
