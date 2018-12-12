b35e6fc3ff958466dee19a956705f40e15f50e30
3zZhou <treezyzhou@gmail.com>
2018-12-12 17:51:21 +0800

    3z

---
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/StudentgradeRepository.java
@@ -0,0 +1,16 @@
+package sg.iss.wafflescollege.repo;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sg.iss.wafflescollege.model.Studentgrade;
+
+public interface StudentgradeRepository extends JpaRepository<Studentgrade, Integer> {
+	
+	@Query("SELECT s FROM Studentgrade s where s.course.cseId = :cseId")
+	ArrayList<Studentgrade> findStudentgradeByCseId(@Param("cseId") String cseId);
+
+}
