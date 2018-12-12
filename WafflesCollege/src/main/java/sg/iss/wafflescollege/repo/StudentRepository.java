9f02ba5212bcbb4dd35b23e94e1cd978f3edf4ff
BobIqbal <mdiqbal23@live.com>
2018-12-12 12:06:21 +0800

    Student Repo, Service and ServiceImpl

--- WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/StudentRepository.java
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/StudentRepository.java
@@ -1,10 +1,47 @@
 package sg.iss.wafflescollege.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.iss.wafflescollege.model.Student;
-
+import sg.iss.wafflescollege.model.Enrollment;
+import sg.iss.wafflescollege.model.Studentgrade;;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
  
+	@Query("SELECT \r\n" + 
+			"     s.STG_ID, c.CSE_CREDIT, s.STG_GRADE\r\n" + 
+			"FROM\r\n" + 
+			"    wafflescollege.course c,\r\n" + 
+			"    wafflescollege.studentgrade s,\r\n" + 
+			"    wafflescollege.enrollment e\r\n" + 
+			"WHERE\r\n" + 
+			"    e.STU_ID = s.STU_ID\r\n" + 
+			"        AND e.ENR_STATUS = 'Completed'\r\n" + 
+			"        AND c.CSE_ID = e.CSE_ID\r\n" + 
+			"        AND c.CSE_ID = s.CSE_ID\r\n" + 
+			"        AND e.STU_ID = :stuID\r\n" + 
+			"\r\n" + 
+			"ORDER BY s.STG_ID")
+	Double[] GPACourseCredits(@Param("stuID") String stuID);
+
+	@Query("SELECT \r\n" + 
+			"     s.STG_ID, c.CSE_CREDIT, s.STG_GRADE\r\n" + 
+			"FROM\r\n" + 
+			"    wafflescollege.course c,\r\n" + 
+			"    wafflescollege.studentgrade s,\r\n" + 
+			"    wafflescollege.enrollment e\r\n" + 
+			"WHERE\r\n" + 
+			"    e.STU_ID = s.STU_ID\r\n" + 
+			"        AND e.ENR_STATUS = 'Completed'\r\n" + 
+			"        AND c.CSE_ID = e.CSE_ID\r\n" + 
+			"        AND c.CSE_ID = s.CSE_ID\r\n" + 
+			"        AND e.STU_ID = :stuID\r\n" + 
+			"\r\n" + 
+			"ORDER BY s.STG_ID")
+	String[] GPAGrades(@Param("stuID") String stuID);
+	
 }
--- WafflesCollege/src/main/java/sg/iss/wafflescollege/services/StudentService.java
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/services/StudentService.java
@@ -2,10 +2,23 @@ package sg.iss.wafflescollege.services;
 
 import java.util.ArrayList;
 
+import sg.iss.wafflescollege.model.Course;
 import sg.iss.wafflescollege.model.Student;
 
 public interface StudentService {
 
+	Double CalculateCGPAByStudentID(String studentID);
+	
+	Double convertGradeToGPA(String grade);
+
+	ArrayList<Course> getEnrolledCoursesList(String studentID, String status);
+
+	ArrayList<Course> getNewCourses(Student s);
+
+	int enrollIntoCourse(Student s, String courseID);
+
+	void viewCourseDetails(String courseID);
+	
 	ArrayList<Student> findAllStudents();
 
 	Student findStudent(String nric);
@@ -17,5 +30,4 @@ public interface StudentService {
 	void removeStudent(Student s);
 
 	ArrayList<Student> findStudentsByCriteria(Student student);
-
 }
