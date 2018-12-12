98db6d6f38bda4a729233f64a74e4c9631d8c593
yaoyao96 <jyyaoyaoyao@gmail.com>
2018-12-12 08:09:39 +0800

    morning

--- WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/EnrollmentRepository.java
+++ WafflesCollege/src/main/java/sg/iss/wafflescollege/repo/EnrollmentRepository.java
@@ -1,9 +1,21 @@
 package sg.iss.wafflescollege.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import sg.iss.wafflescollege.model.Enrollment;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Integer> {
 
+	@Query("select e from Enrollment e where e.enrStatus ='Pending'")
+	ArrayList<Enrollment> findAllNewEnrollments();
+	
+	@Query("select e from Enrollment e where e.enrStatus ='Pending' and e.stuId=:sid")
+	ArrayList<Enrollment> findNewEnrollmentBysID(@Param("sid") String sid);
+
+	@Query("select e from Enrollment e where e.enrStatus ='Pending'e.stuId=:sid")
+	ArrayList<Enrollment> findEnrollmentBysID(@Param("sid") String sid);
 }
